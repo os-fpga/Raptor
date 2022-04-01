1b95cbcecc787ab887875d88402e735894f60616
Aram Kostanyan <aram@rapidsilicon.com>
2022-04-01 21:18:04 +0500

    Updated dependencies.

--- .github/workflows/install_ubuntu_dependencies_build.sh
+++ .github/workflows/install_ubuntu_dependencies_build.sh
@@ -13,7 +13,25 @@ sudo apt install -y \
   valgrind \
   xorg \
   qtbase5-dev qtchooser qt5-qmake qtbase5-dev-tools qtdeclarative5-dev \
-  xvfb
+  xvfb \
+  tcllib \
+  bison \
+  flex \
+  libreadline-dev \
+  gawk \
+  tcl-dev \
+  libffi-dev \
+  git \
+  graphviz \
+  xdot \
+  pkg-config \
+  python3 \
+  libboost-system-dev \
+  libboost-python-dev \
+  libboost-filesystem-dev \
+  zlib1g-dev \
+  ninja-build 
+  
 sudo ln -sf /usr/bin/g++-9 /usr/bin/g++
 sudo ln -sf /usr/bin/gcc-9 /usr/bin/gcc
 sudo ln -sf /usr/bin/gcov-9 /usr/bin/gcov
