fae172bd75a124ad97ca9b539e89e229da7ef465
Aram Kostanyan <aram@rapidsilicon.com>
2022-04-04 23:47:10 +0500

    Added 'bison' build.

--- .github/workflows/install_macos_dependencies_build.sh
+++ .github/workflows/install_macos_dependencies_build.sh
@@ -8,6 +8,6 @@ brew install qt5 \
     bash \
     readline \
     ninja
-export PATH="/usr/local/opt/qt@5/bin:$PATH"
+export PATH="/usr/local/opt/qt@5/bin:/usr/local/opt/bison/bin:$PATH"
 export LDFLAGS="-L/usr/local/opt/qt@5/lib"
 export CPPFLAGS="-I/usr/local/opt/qt@5/include"
