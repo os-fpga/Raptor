31568ac07cabdc26d84b271be30f17b730845d70
alain-rs <90641725+alain-rs@users.noreply.github.com>
2022-04-04 16:16:56 -0700

    Alain centos (#17)
    * centos build
    
    * centos build
    
    * centos build
    
    Co-authored-by: Alain Dargelas <alain.dargelas@gmail.com>

--- .github/workflows/install_centos_dependencies_build.sh
+++ .github/workflows/install_centos_dependencies_build.sh
@@ -14,6 +14,9 @@ scl enable devtoolset-9 bash
 yum install -y tcl
 yum install -y tcl-dev
 yum install -y make
+yum install -y flex
+yum install -y bison
+yum install -y readline-devel
 yum install -y swig
 yum install -y which
 yum install -y google-perftools
