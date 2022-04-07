8a7e1ca55d59dac74b372fed582f522fc54d2b5f
alain-rs <90641725+alain-rs@users.noreply.github.com>
2022-04-06 18:08:52 -0700

    Yosys and OpenFPGA build (#18)
    * Updated yosys_verific_rs submodule and included it into CMake.
    
    * Adding missing file from previous commit.
    
    * Updated dependencies.
    
    * Updated 'yosys_verific_rs' submodule.
    
    * Updated 'yosys_verific_rs' submodule.
    
    * Updated dependencies for CentOS and yosys_verific_rs submodule.
    
    * Updated 'yosys_verific_rs' submodule.
    
    * Added 'bison' build.
    
    * Added 'bison' into PATH.
    
    * Test bison version.
    
    * Fix synthax.
    
    * .github/workflows/main.yml
    
    * Install additional dependencies.
    
    * Enable full CI.
    
    * Restricted CI to only macos.
    
    * Upload build dir as artifact for debugging.
    
    * Continue on build failure.
    
    * Added GNU coreutils as dependancy.
    
    * Chenged Github Path setting.
    
    * Updated yosys_verific_rs submodule.
    
    * Updated 'yosys_verific_rs' submodule and removed continue-on-fail flag.
    
    * Enable full flow.
    
    * Updated OpenFPGA_RS submodule.
    
    * Updated 'yosys_verific_rs' submodule and upgraded gcc for macos.
    
    * set CC and CXX vars.
    
    * Check gcc
    
    * set gcc
    
    * Disabled OpenFPGA build on APPLE
    
    Co-authored-by: Aram Kostanyan <aram@rapidsilicon.com>

--- .github/workflows/install_macos_dependencies_build.sh
+++ .github/workflows/install_macos_dependencies_build.sh
@@ -1,5 +1,14 @@
 # Install required dependencies for Mac OS systems
-brew install qt5
+brew install bison \
+    flex \
+    gawk \
+    libffi \
+    pkg-config \
+    bash \
+    readline \
+    ninja \
+    wget \
+    coreutils
 export PATH="/usr/local/opt/qt@5/bin:$PATH"
 export LDFLAGS="-L/usr/local/opt/qt@5/lib"
 export CPPFLAGS="-I/usr/local/opt/qt@5/include"
--- .github/workflows/install_ubuntu_dependencies_build.sh
+++ .github/workflows/install_ubuntu_dependencies_build.sh
@@ -14,7 +14,24 @@ sudo apt install -y \
   valgrind \
   xorg \
   qtbase5-dev qtchooser qt5-qmake qtbase5-dev-tools qtdeclarative5-dev \
-  xvfb
+  xvfb \
+  tcllib \
+  bison \
+  flex \
+  libreadline-dev \
+  gawk \
+  libffi-dev \
+  git \
+  graphviz \
+  xdot \
+  pkg-config \
+  python3 \
+  libboost-system-dev \
+  libboost-python-dev \
+  libboost-filesystem-dev \
+  zlib1g-dev \
+  ninja-build 
+  
 sudo ln -sf /usr/bin/g++-9 /usr/bin/g++
 sudo ln -sf /usr/bin/gcc-9 /usr/bin/gcc
 sudo ln -sf /usr/bin/gcov-9 /usr/bin/gcov
