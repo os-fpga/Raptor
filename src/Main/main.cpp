5080c955c419639bf694743e1bf1e1b725f09a5e
alain-rs <90641725+alain-rs@users.noreply.github.com>
2022-04-29 13:32:18 -0700

    version and verilog out (#42)
    * Raptor compiler update
    
    * Raptor compiler update
    
    * Raptor compiler update
    
    * Raptor compiler update
    
    * RaptorRS compiler
    
    * Update tests
    
    * version
    
    * format
    
    Co-authored-by: Alain Dargelas <alain.dargelas@gmail.com>

--- src/Main/main.cpp
+++ src/Main/main.cpp
@@ -6,6 +6,7 @@
 #include "Compiler/CompilerRS.h"
 #include "Main/CommandLine.h"
 #include "Main/Foedag.h"
+#include "Main/Settings.h"
 #include "Main/ToolContext.h"
 #include "MainWindow/Session.h"
 #include "MainWindow/main_window.h"
@@ -39,6 +40,8 @@ int main(int argc, char** argv) {
   FOEDAG::ToolContext* context =
       new FOEDAG::ToolContext(ToolName, Company, ExecutableName);
 
+  FOEDAG::Settings* settings = new FOEDAG::Settings();
+
   FOEDAG::Compiler* compiler = nullptr;
   FOEDAG::CompilerRS* opcompiler = nullptr;
   if (cmd->CompilerName() == "dummy") {
@@ -49,8 +52,9 @@ int main(int argc, char** argv) {
     compiler->SetUseVerific(true);
   }
 
-  FOEDAG::Foedag* foedag = new FOEDAG::Foedag(
-      cmd, RS::mainWindowBuilder, RS::registerAllCommands, compiler, context);
+  FOEDAG::Foedag* foedag =
+      new FOEDAG::Foedag(cmd, RS::mainWindowBuilder, RS::registerAllCommands,
+                         compiler, settings, context);
 
   if (opcompiler) {
     const std::string& binpath = foedag->Context()->BinaryPath().string();
