e8a081404f76a70bf912c6f7d343198a76a10b8c
alain-rs <90641725+alain-rs@users.noreply.github.com>
2022-05-16 15:17:05 -0700

    bitstream gen fixes (#70)
    * example design and arch files
    
    * revert event processing
    
    * Support gate level input
    
    * Support gate level input
    
    * Update all subsystems
    
    * Fix missing vpr and openfpga exec in MacOS
    
    * Fix missing vpr and openfpga exec in MacOS
    
    * Fix missing vpr and openfpga exec in MacOS
    
    * Fix missing vpr and openfpga exec in MacOS
    
    * Fix missing vpr and openfpga exec in MacOS
    
    * Fix missing vpr and openfpga exec in MacOS
    
    * Fix missing vpr and openfpga exec in MacOS
    
    * New Bitstream generation commands
    
    * Constraint support
    
    * Project API, early sdc error
    
    * libvpr.so path, bitstream reuses vpr data
    
    * Reorg CI tests
    
    * bitstream gen fixes
    
    * Raptor version
    
    Co-authored-by: Alain Dargelas <alain.dargelas@gmail.com>

---
+++ tests/Testcases/sasc_testcase/raptor.tcl
@@ -0,0 +1,13 @@
+create_design sasc
+architecture ./TSMC22nm_vpr.xml ./TSMC22nm_openfpga.xml
+bitstream_config_files -bitstream ../../Arch/bitstream_annotation_empty.xml -sim ./fixed_sim_openfpga.xml -repack ./repack_design_constraint.xml
+add_design_file -V_2001 ./rtl/timescale.v ./rtl/sasc_brg.v ./rtl/sasc_fifo4.v ./rtl/sasc.v
+set_top_module sasc
+set_device_size 16x16
+add_constraint_file constraints.sdc
+synthesize
+packing
+place
+route
+sta
+bitstream force
