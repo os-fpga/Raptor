deed49de9e57ba1a813e10907db0ae12ebdfd19f
alain-rs <90641725+alain-rs@users.noreply.github.com>
2022-05-20 22:23:13 -0700

    add synthesis_type command, remove rs_synthesis (#80)
    * example design and arch files
    
    * revert event processing
    
    * Support gate level input
    
    * Support gate level input
    
    * Update all subsystems
    
    * Fix missing vpr and openfpga exec in MacOS
    
    * Fix missing vpr and openfpga exec in MacOS
    
    * Fix missing vpr and openfpga exec in MacOS
    
    * Fix missing vpr and openfpga exec in MacOS
    
    * Fix missing vpr and openfpga exec in MacOS
    
    * Fix missing vpr and openfpga exec in MacOS
    
    * Fix missing vpr and openfpga exec in MacOS
    
    * New Bitstream generation commands
    
    * Constraint support
    
    * Project API, early sdc error
    
    * libvpr.so path, bitstream reuses vpr data
    
    * Reorg CI tests
    
    * bitstream gen fixes
    
    * Raptor version
    
    * disable settings
    
    * revert settings
    
    * WIP device xml
    
    * as-is contraints, file browser, subprocess errors
    
    * respect (again) file order
    
    * target_device command
    
    * target_device command
    
    * update foedag
    
    * add synthesis_type command, remove rs_synthesis
    
    Co-authored-by: Alain Dargelas <alain.dargelas@gmail.com>

--- tests/Testcases/aes_decrypt_fpga/aes_decrypt_open_source.tcl
+++ tests/Testcases/aes_decrypt_fpga/aes_decrypt_open_source.tcl
@@ -3,7 +3,7 @@ set_top_module decrypt
 architecture ../../Arch/k6_frac_N10_tileable_40nm.xml
 set_device_size 76x76
 verific_parser off
-rs_synthesis off
+synthesis_type Yosys
 add_design_file aes_decrypt128.sv aes_decrypt256.sv  gfmul.sv InvMixCol_slice.sv InvSbox.sv InvSubBytes.sv KeyExpand192.sv  KschBuffer.sv  Sbox.sv aes_decrypt192.sv  decrypt.sv InvAddRoundKey.sv InvMixColumns.sv  InvShiftRows.sv KeyExpand128.sv KeyExpand256.sv RotWord.sv  SubWord.sv  -SV_2012
 add_design_file generic_muxfx.v
 add_constraint_file aes_decrypt.sdc
