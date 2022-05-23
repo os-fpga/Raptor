d94d54ac9e54f383e79620dc64d64eac648aa33d
Alain Dargelas <alain.dargelas@gmail.com>
2022-05-22 22:30:08 -0700

    example

--- tests/Testcases/and2_testcase/raptor.tcl
+++ tests/Testcases/and2_testcase/raptor.tcl
@@ -1,18 +1,23 @@
-create_design and2
+# Device target
 target_device MPW1
-verific_parser on 
+set_device_size 4x4
+
+# Design
+create_design and2
 add_design_file -V_2001 ./rtl/and2.v
 set_top_module and2
-set_device_size 4x4
 add_constraint_file constraints.sdc
+
+# Compilation
 synthesis_type QL
 synth_options -no_dsp -no_adder -no_bram
-
 synthesize
+
 packing
 
 pnr_options --fix_pins ./pinmap.place
 place
+
 route
 sta
 bitstream force
