9000284d4a2d75e701f93d5ff6f1a532440e20d4
alain-rs <90641725+alain-rs@users.noreply.github.com>
2022-07-01 21:53:33 -0700

    litex support wip (#130)
    * Update subsystems
    
    * update mpw1 vpr file
    
    * update mpw1 vpr file
    
    * litex support wip
    
    * litex support wip
    
    Co-authored-by: Alain Dargelas <alain.dargelas@gmail.com>

---
+++ tests/Testcases/ip_gen_axis_conv/use_ip.v
@@ -0,0 +1,24 @@
+`timescale 1 ps/ 1 ps 
+///////////////////////////////////////////////////////////////// 
+// Company: 
+// Engineer: 
+// Create Date: 2022-07-01 16:34:14
+// Design Name: 
+// Module Name: use_ip
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision: 
+// Additional Comments:
+// 
+///////////////////////////////////////////////////////////////// 
+
+
+module use_ip( 
+
+    ); 
+endmodule 
