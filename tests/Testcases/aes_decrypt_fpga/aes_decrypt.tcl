b357517fd938ebf5f9e252216420f7dfe4e97bcd
Alain Dargelas <alain.dargelas@gmail.com>
2022-07-07 17:51:58 -0700

    reverting bitstream opt

--- tests/Testcases/aes_decrypt_fpga/aes_decrypt.tcl
+++ tests/Testcases/aes_decrypt_fpga/aes_decrypt.tcl
@@ -5,7 +5,7 @@ set_top_module decrypt
 add_design_file aes_decrypt128.sv aes_decrypt256.sv  gfmul.sv InvMixCol_slice.sv InvSbox.sv InvSubBytes.sv KeyExpand192.sv  KschBuffer.sv  Sbox.sv aes_decrypt192.sv  decrypt.sv InvAddRoundKey.sv InvMixColumns.sv  InvShiftRows.sv KeyExpand128.sv KeyExpand256.sv RotWord.sv  SubWord.sv  -SV_2012
 add_design_file generic_muxfx.v
 add_constraint_file aes_decrypt.sdc
-synth_options -effort low
+synth_options -effort high
 synthesize
 packing
 place
