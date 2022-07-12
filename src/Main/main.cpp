85c152a0e528de9a859824d93b6a1025cdc4518a
Alain Dargelas <alain.dargelas@gmail.com>
2022-07-12 15:54:40 -0700

    About , VPR GUI

--- src/Main/main.cpp
+++ src/Main/main.cpp
@@ -20,7 +20,10 @@ namespace RS {
 
 QWidget* mainWindowBuilder(FOEDAG::Session* session) {
   FOEDAG::MainWindow* mainW = new FOEDAG::MainWindow{session};
-  mainW->MainWindowTitle(std::string(Company) + " " + std::string(ToolName));
+  auto info = mainW->Info();
+  info.name = QString("%1 %2").arg(Company, ToolName);
+  info.url = "https://github.com/RapidSilicon/Raptor/commit/";
+  mainW->Info(info);
   return mainW;
 }
 
