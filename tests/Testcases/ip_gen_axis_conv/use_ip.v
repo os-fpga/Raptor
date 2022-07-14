a046d19ae7b4fd08a869ab6b78456129356aa503
alain-rs <90641725+alain-rs@users.noreply.github.com>
2022-07-13 17:28:57 -0700

    IP gen, pin loc example (#150)
    * IP gen, pin loc example
    
    Co-authored-by: Alain Dargelas <alain.dargelas@gmail.com>

--- tests/Testcases/ip_gen_axis_conv/use_ip.v
+++ tests/Testcases/ip_gen_axis_conv/use_ip.v
@@ -1,24 +1,28 @@
 `timescale 1 ps/ 1 ps 
-///////////////////////////////////////////////////////////////// 
-// Company: 
-// Engineer: 
-// Create Date: 2022-07-01 16:34:14
-// Design Name: 
-// Module Name: use_ip
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision: 
-// Additional Comments:
-// 
-///////////////////////////////////////////////////////////////// 
-
 
 module use_ip( 
-
+    input  wire axis_clk,
+    input  wire axis_rst,
+    input  wire axis_in_tvalid,
+    input  wire axis_in_tlast,
+    output wire axis_in_tready,
+    input  wire [31:0] axis_in_tdata,
+    output wire axis_out_tvalid,
+    output wire axis_out_tlast,
+    input  wire axis_out_tready,
+    output wire [15:0] axis_out_tdata
     ); 
+
+    conv32_16 U1 (
+        axis_clk,
+        axis_rst,
+        axis_in_tvalid,
+       axis_in_tlast,
+        axis_in_tready,
+      axis_in_tdata,
+       axis_out_tvalid,
+       axis_out_tlast,
+      axis_out_tready,
+       axis_out_tdata);
+
 endmodule 
