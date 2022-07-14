ab7bef1732acaf15f793c1388d5a05d27a1798de
alain-rs <90641725+alain-rs@users.noreply.github.com>
2022-07-13 17:28:57 -0700

    IP gen, pin loc example (#150)
    * IP gen, pin loc example
    
    Co-authored-by: Alain Dargelas <alain.dargelas@gmail.com>

--- tests/Testcases/ip_gen_axis_conv/raptor.tcl
+++ tests/Testcases/ip_gen_axis_conv/raptor.tcl
@@ -4,5 +4,5 @@ create_design ip_test
 configure_ip axis_converter -mod_name conv32_16 -version 1.0 -Pcore_in_width=32 -Pcore_out_width=16 -Pcore_reverse=1 -out_file rs_ips/conv32_16.v
 ipgenerate
 set_top_module use_ip
-add_design_file rs_ips/conv_32_16.v use_ip.v rs_ips/conv32_16.v
-synth
+add_design_file rs_ips/conv32_16.v use_ip.v
+synth delay
