9340401c9cc256a051c421acc75338d1bdc03f7c
enjoy-digital <florent@enjoy-digital.fr>
2022-07-15 19:48:54 +0200

    Add LiteX installation to CI workflow. (#169)
    * ci/workflows: Add initial LiteX installation (only on Ubuntu for now).
    
    * ci/workflows/litex: Add LiteX installation on CentOS.
    
    * ci/workflows: Avoid touching ccache install (not required with verilator install from packages).
    
    * ci/workflows: Remove sudo in LiteX installation on CentOS.

---
+++ .github/workflows/install_litex_ubuntu.sh
@@ -0,0 +1,34 @@
+# Install required dependencies for Ubuntu systems
+sudo apt-get update -qq
+sudo apt install -y \
+  build-essential \
+  python3 \
+  ninja-build \
+  libevent-dev \
+  libjson-c-dev \
+  flex \
+  bison \
+  libfl-dev \
+  libfl2 \
+  verilator \
+  zlibc \
+  zlib1g-dev
+
+# Install required Python3 packages.
+pip3 install \
+  setuptools \
+  requests \
+  pexpect \
+  meson
+
+
+# Download/Install LiteX.
+wget https://raw.githubusercontent.com/enjoy-digital/litex/master/litex_setup.py
+chmod +x litex_setup.py
+./litex_setup.py --init
+sudo ./litex_setup.py --install
+
+# Download/Install RISC-V GCC toolchain.
+./litex_setup.py --gcc=riscv
+sudo mkdir /usr/local/riscv
+sudo cp -r riscv64-*/* /usr/local/riscv
