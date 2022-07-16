311ad8cd1d66a60e1dfd84165d28db2fd8396c1a
Alain Dargelas <alain.dargelas@gmail.com>
2022-07-15 22:10:11 -0700

    License for devices

--- tests/Testcases/ip_gen_axis_conv/raptor.tcl
+++ tests/Testcases/ip_gen_axis_conv/raptor.tcl
@@ -1,5 +1,13 @@
 target_device GEMINI
 add_litex_ip_catalog ip_generators/
+puts "Catalog:"
+foreach ip [ip_catalog] {
+    puts "IP: $ip"
+    foreach param [ip_catalog $ip] {
+        puts "  PARAM [lindex $param 0], default: [lindex $param 1]"
+    }
+}
+
 create_design ip_test
 configure_ip axis_converter -mod_name conv32_16 -version 1.0 -Pcore_in_width=32 -Pcore_out_width=16 -Pcore_reverse=1 -out_file rs_ips/conv32_16.v
 ipgenerate
