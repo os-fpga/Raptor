5734b0e9779a88e4459a755f3e09f8f48e17e8e1
Alain Dargelas <alain.dargelas@gmail.com>
2022-08-01 21:33:09 -0700

    Verilog netlist reader (off by default)

--- tests/Testcases/aes_decrypt_fpga/aes_decrypt.tcl
+++ tests/Testcases/aes_decrypt_fpga/aes_decrypt.tcl
@@ -4,6 +4,7 @@ create_design AES_DECRYPT
 set_top_module wrapper
 add_design_file aes_decrypt128.sv aes_decrypt256.sv  gfmul.sv InvMixCol_slice.sv InvSbox.sv InvSubBytes.sv KeyExpand192.sv  KschBuffer.sv  Sbox.sv aes_decrypt192.sv  decrypt.sv InvAddRoundKey.sv InvMixColumns.sv  InvShiftRows.sv KeyExpand128.sv KeyExpand256.sv RotWord.sv  SubWord.sv  -SV_2012
 add_design_file generic_muxfx.v wrapper.v
+
 add_constraint_file aes_decrypt.sdc
 synth_options -effort high 
 synthesize area
