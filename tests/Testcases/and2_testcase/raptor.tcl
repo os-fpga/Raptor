84c9cff238a47508396d489e1b2af6976ea47697
Ravikiran Chollangi <ravikiran.chollangi@rapidsilicon.com>
2022-08-18 15:51:25 -0700

    Sprint 2 and partial Sprint 2 foedag updates

--- tests/Testcases/and2_testcase/raptor.tcl
+++ tests/Testcases/and2_testcase/raptor.tcl
@@ -1,12 +1,12 @@
-# Device target
-target_device MPW1
-set_device_size 4x4
-
 # Design
 create_design and2
 add_design_file -V_2001 ./rtl/and2.v
 set_top_module and2
 add_constraint_file constraints.sdc
+
+# Device target
+target_device MPW1
+set_device_size 4x4
 
 # Compilation
 synthesize delay
