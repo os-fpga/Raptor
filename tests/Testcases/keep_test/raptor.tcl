c474ca29848fbac672af37826c86e85807a21d22
Alain Dargelas <alain.dargelas@gmail.com>
2022-08-22 15:56:24 -0700

    keep command

---
+++ tests/Testcases/keep_test/raptor.tcl
@@ -0,0 +1,12 @@
+# Design
+create_design keep_test
+add_design_file -V_2001 ./rtl/top.v
+set_top_module top
+add_constraint_file constraints.sdc
+
+# Device target
+target_device GEMINI
+
+# Compilation
+synthesize 
+packing
