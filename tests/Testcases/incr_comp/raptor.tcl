13ed7edadc2acbdb48e74dfba1020875d4963f41
KochynVolodymyr <95262932+KochynVolodymyr@users.noreply.github.com>
2022-09-06 21:30:23 +0300

    Add test for GEMINIEDA-324 (#239)

---
+++ tests/Testcases/incr_comp/raptor.tcl
@@ -0,0 +1,26 @@
+set script_path [ file dirname [ file normalize [ info script ] ] ]
+source $script_path/copy.tcl
+
+set projName incr_comp
+set tempFile $projName/tmp.v
+
+create_design $projName
+target_device GEMINI
+
+file copy -force $script_path/and2.v $tempFile
+
+add_design_file $tempFile
+set_top_module and2
+synthesize clean # cleanup before test
+synthesize
+
+file copy -force $projName/incr_comp_post_synth.blif $projName/incr_comp_post_synth.blif1
+# wait 1 sec since mtime will be the same for $tempFile and *.blif
+after 1000
+copyContent $script_path/and2_modified.v $tempFile
+synthesize
+file copy -force $projName/incr_comp_post_synth.blif $projName/incr_comp_post_synth.blif2
+set diffresult [catch {exec sh -c "diff $projName/incr_comp_post_synth.blif1 $projName/incr_comp_post_synth.blif2"} diffresult] 
+if {$diffresult == 0} {
+    error "Synthesis didn't resynthesized"
+}
