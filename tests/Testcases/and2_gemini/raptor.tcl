7d13c1df1e92c6f81d3e1f658f91cb54ab7514e0
Alain Dargelas <alain.dargelas@gmail.com>
2022-09-07 13:52:28 -0700

    Enable analyze step

--- tests/Testcases/and2_gemini/raptor.tcl
+++ tests/Testcases/and2_gemini/raptor.tcl
@@ -1,11 +1,13 @@
 # Design
 create_design and2_gemini
-# Device target
-target_device GEMINI
 add_design_file -V_2001 ./rtl/and2.v
 set_top_module and2
 add_constraint_file constraints.sdc
+
+# Device target
+target_device GEMINI
 # Compilation
+analyze
 synthesize delay
 packing
 place
