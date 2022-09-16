1530336d1bb515ff39e7efd47fe834a6350b4916
Bilal Ahmed <bilal.ahmed@rslpt44.rapidsilicon.local>
2022-09-16 17:56:58 +0500

    Python Path Updated

--- src/.raptorenv_lin64.sh
+++ src/.raptorenv_lin64.sh
@@ -26,7 +26,7 @@ else
 fi
 
 if [ -n "${PYTHONPATH}" ]; then
-	export PYTHONPATH=$RAPTOR_PATH/share/litex_rs/:$PYTHONPATH/$RAPTOR_PATH/share/raptor/IP_Catalog/:$PYTHONPATH/$RAPTOR_PATH/share/lib/python3.8/site-packages/:$PYTHONPATH/$RAPTOR_PATH/share/lib/python3./site-packages/:$PYTHONPATH
+	export PYTHONPATH=$RAPTOR_PATH/share/litex_rs/:$PYTHONPATH/$RAPTOR_PATH/share/raptor/IP_Catalog/:$PYTHONPATH/$RAPTOR_PATH/share/raptor/lib/python3.8/site-packages:$PYTHONPATH
 else
-	export PYTHONPATH=$RAPTOR_PATH/share/litex_rs/:$RAPTOR_PATH/share/raptor/IP_Catalog/:/$RAPTOR_PATH/share/lib/python3.8/site-packages/:/$RAPTOR_PATH/share/lib/python3./site-packages/
+	export PYTHONPATH=$RAPTOR_PATH/share/litex_rs/:$RAPTOR_PATH/share/raptor/IP_Catalog/:$RAPTOR_PATH/share/raptor/lib/python3.8/site-packages/
 fi
