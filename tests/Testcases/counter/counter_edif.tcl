4cd41c4fa8ce82e712c209d27593ff15dded2519
behzadmehmood-rs <behzad.mahmood@rapidsilicon.com>
2022-09-23 11:15:18 +0500

    Updated OpenFPGA submodule

--- tests/Testcases/counter/counter_edif.tcl
+++ tests/Testcases/counter/counter_edif.tcl
@@ -1,19 +1,16 @@
 # Design
-create_design counter_8bit
-add_design_file -V_2001 counter_8bit.v
-set_top_module counter_8bit
-#add_constraint_file constraints.sdc
+create_design counter
+add_design_file -V_2001 counter.v
+set_top_module counter
+add_constraint_file constraints.sdc
 
 # Device target
 target_device MPW1
-
+set_device_size 4x4
 
 # Compilation
 synthesize delay
-custom_openfpga_script ./counter_8bit.openfpga
 pnr_netlist_lang edif
-set_device_size 4x4
-
 packing
 place
 route
