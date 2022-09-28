0523e804a605b3e110ba53da1ad977b2b15d15d3
nadeemyaseen-rs <86344264+nadeemyaseen-rs@users.noreply.github.com>
2022-09-28 19:21:43 +0500

    Update install_litex_ubuntu.sh

--- .github/workflows/install_litex_ubuntu.sh
+++ .github/workflows/install_litex_ubuntu.sh
@@ -3,5 +3,5 @@ sudo apt-get update
 sudo apt install -y cmake build-essential
 sudo apt install -y python3 python3-pip
 python3 -m pip install --upgrade pip
-python3 -m pip install pipenv
+python3 -m pip install pipenv wheel
 
