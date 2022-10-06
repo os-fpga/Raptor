bd3c381aee0af9322058954fd4933423683438f7
Alain Dargelas <alain.dargelas@gmail.com>
2022-10-06 14:28:04 -0700

    Gemini December layout

--- tests/Testcases/device_size_negative/raptor.tcl
+++ tests/Testcases/device_size_negative/raptor.tcl
@@ -2,7 +2,7 @@
 create_design and2_gemini
 # Device target
 target_device GEMINI
-set_device_size 4x4
+set_device_size 42x43
 add_design_file -V_2001 ./rtl/and2.v
 set_top_module and2
 add_constraint_file constraints.sdc
