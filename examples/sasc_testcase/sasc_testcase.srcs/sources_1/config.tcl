45c2335b40301cfafddb9a489e292d42130bfa58
TarasSydor <109239890+TarasSydor@users.noreply.github.com>
2022-10-13 01:19:12 +0300

    [RG-114] Introduced "examples" folder with *.ospr based projects (#336)
    * [RG-114] Introduced "examples" folder with *.osrp based projects
    
    * Update Makefile as tcl-based examples are no longer in place
    
    * Get back tcl tests. Put them into tcl_examples folder
    
    * Sprint 6.2 FOEDAG updates (#334)
    
    * Updated yosys_verific_rs submodule (#335)
    
    * Updated yosys_verific_rs submodule.
    
    * Updated yosys_verific_rs submodule.
    
    * encrypt update (#337)
    
    Co-authored-by: Alain Dargelas <alain.dargelas@gmail.com>
    
    * Cmake updated for the encryption tool (#324)
    
    * Cmake updated for the encryption tool
    
    * bump Raptor_Tools and Backend submodule
    
    Co-authored-by: Nadeem Yaseen <nyaseen@sw05.rapid.local>
    
    * raptor version (#338)
    
    * encrypt update
    
    * version
    
    Co-authored-by: Alain Dargelas <alain.dargelas@gmail.com>
    
    * Update main.yml
    
    * bump Raptor_Tools
    
    * revert the condition
    
    * Sprint 6.3 bug fixes (#339)
    
    * ip catalog update (#342)
    
    Co-authored-by: Alain Dargelas <alain.dargelas@gmail.com>
    
    * Tests for IP Catalog (#307)
    
    * Tests for IP Catalog added in Tests_IPs
    
    * Make file updated to run tests.
    
    * Tcl for all IPs updated to cater IP Catalog trests
    
    * Directory updated
    
    * IP Catalog submodule updated.
    
    * Makefile updated as per new directory TestIP
    
    Co-authored-by: Bilal Ahmed <bilal.ahmed@rslpt44.rapidsilicon.local>
    Co-authored-by: BILAL AHMED <bahmed@sw01.rapidsilicon.local>
    
    * Litex reference designs updated with native RTL/AXI design (#341)
    
    Co-authored-by: Bilal Ahmed <bilal.ahmed@rslpt44.rapidsilicon.local>
    
    * version (#343)
    
    * version
    
    * version
    
    Co-authored-by: Alain Dargelas <alain.dargelas@gmail.com>
    
    Co-authored-by: ravic-rs <108307503+ravic-rs@users.noreply.github.com>
    Co-authored-by: aram-rs <90259780+aram-rs@users.noreply.github.com>
    Co-authored-by: alain-rs <90641725+alain-rs@users.noreply.github.com>
    Co-authored-by: Alain Dargelas <alain.dargelas@gmail.com>
    Co-authored-by: Muhammad Raza <87646228+Raza-jafari-RS@users.noreply.github.com>
    Co-authored-by: Nadeem Yaseen <nyaseen@sw05.rapid.local>
    Co-authored-by: nadeemyaseen-rs <86344264+nadeemyaseen-rs@users.noreply.github.com>
    Co-authored-by: nadeemyaseen-rs <nadeem.yaseen@rapidsilicon.com>
    Co-authored-by: Bilal Ahmed <96869856+bilalahmed-RS@users.noreply.github.com>
    Co-authored-by: Bilal Ahmed <bilal.ahmed@rslpt44.rapidsilicon.local>
    Co-authored-by: BILAL AHMED <bahmed@sw01.rapidsilicon.local>

---
+++ examples/sasc_testcase/sasc_testcase.srcs/sources_1/config.tcl
@@ -0,0 +1,6 @@
+set ::env(PROJECT_NAME) sasc
+set ::env(DESIGN_TOP) sasc
+set ::env(DESIGN_DIR) RTL_Benchmark/Verilog/iwls2005_designs/sasc/rtl
+set ::env(TOP_VERILOG) RTL_Benchmark/Verilog/iwls2005_designs/sasc/rtl/sasc.v
+set ::env(CLOCK_COUNT) 1
+set ::env(DOMAIN) controller
