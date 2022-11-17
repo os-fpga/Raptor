e1f67935b8747f0862adebf4696317be713f2a01
nadeemyaseen-rs <86344264+nadeemyaseen-rs@users.noreply.github.com>
2022-11-17 21:50:39 +0500

    Update install_ubuntu_dependencies_build.sh

--- .github/workflows/install_ubuntu_dependencies_build.sh
+++ .github/workflows/install_ubuntu_dependencies_build.sh
@@ -42,3 +42,5 @@ sudo apt -y install lsb-core
 sudo ln -sf /usr/bin/g++-9 /usr/bin/g++
 sudo ln -sf /usr/bin/gcc-9 /usr/bin/gcc
 sudo ln -sf /usr/bin/gcov-9 /usr/bin/gcov
+sudo ln -sf /usr/lib/x86_64-linux-gnu/libffi.so.7.1.0 /usr/lib/x86_64-linux-gnu/libffi.so.6
+
