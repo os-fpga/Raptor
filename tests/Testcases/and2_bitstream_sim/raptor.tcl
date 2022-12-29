087b3fb7759623aa2312348962cb03dfc48d3cac
alain-rs <90641725+alain-rs@users.noreply.github.com>
2022-12-29 06:23:17 -0800

    clear_simulation_files command (#549)
    Co-authored-by: Alain Dargelas <alain.dargelas@gmail.com>

--- tests/Testcases/and2_bitstream_sim/raptor.tcl
+++ tests/Testcases/and2_bitstream_sim/raptor.tcl
@@ -5,8 +5,8 @@ set_top_module and2
 add_constraint_file constraints.sdc
 
 # Testbench
-#add_simulation_file testbench.sv
-#set_top_testbench tb_and2
+add_simulation_file testbench.sv
+set_top_testbench tb_and2
 
 # Device target
 target_device GEMINI_4x4
@@ -35,6 +35,7 @@ sta
 bitstream enable_simulation
 
 # Bitstream Simulation
+clear_simulation_files
 add_simulation_file SRC/CustomModules/rs_mmff.v SRC/cells.v 
 add_library_path ../../Castor_V2/IPs/openfpga-pd-castor-rs/k6n8_TSMC16nm_7.5T/FPGA10x8_dp_castor_pnr/release/FPGA10x8_dp_castor_verilog/SRC/submodules/
 simulate "bitstream_bd" "icarus" 
