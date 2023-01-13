892a2d26a9bfc8e1f925d351ae38180ebe41397e
Alain Dargelas <alain.dargelas@gmail.com>
2023-01-13 10:33:45 -0800

    fix testcase for opensta

--- tests/Testcases/param_counter/raptor.tcl
+++ tests/Testcases/param_counter/raptor.tcl
@@ -4,6 +4,7 @@ add_include_path ./rtl
 add_library_path ./rtl
 add_design_file -SV_2009 ./rtl/param_up_counter.v
 set_top_module param_up_counter
+add_constraint_file constraints.sdc
 
 # Testbench
 add_simulation_file sim_main.cpp
@@ -30,7 +31,7 @@ route
 simulate pnr counter_pnr.fst
 
 # Static Timing Analysis
-sta
+sta opensta
 
 # Bitstream Generation
 bitstream
