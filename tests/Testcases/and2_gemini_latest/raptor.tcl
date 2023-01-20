3b933c6ca5e663d77b29b363a85843f6e471bddb
Alain Dargelas <alain.dargelas@gmail.com>
2023-01-20 11:26:59 -0800

    Updated testcase to latest pin table

--- tests/Testcases/and2_gemini_latest/raptor.tcl
+++ tests/Testcases/and2_gemini_latest/raptor.tcl
@@ -3,6 +3,7 @@ create_design and2_gemini_latest
 add_design_file -V_2001 ./rtl/and2.v
 set_top_module and2
 add_constraint_file constraints.sdc
+add_constraint_file pin_mapping.pin
 
 # Device target
 target_device GEMINI
