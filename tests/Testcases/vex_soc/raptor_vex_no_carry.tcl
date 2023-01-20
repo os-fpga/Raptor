ad3256cb6685fff1f8fef0cc2f18cb36566c6842
alain-rs <90641725+alain-rs@users.noreply.github.com>
2023-01-19 19:48:13 -0800

    Last pin table (#593)
    * latest pin table for March release
    
    * latest pin table for March release
    
    Co-authored-by: Alain Dargelas <alain.dargelas@gmail.com>

--- tests/Testcases/vex_soc/raptor_vex_no_carry.tcl
+++ tests/Testcases/vex_soc/raptor_vex_no_carry.tcl
@@ -4,6 +4,7 @@ add_include_path ./
 add_library_path rtl/
 add_library_ext .v .sv
 add_design_file rtl/vex_soc.v
+add_constraint_file constraints.sdc
 set_top_module vex_soc
 synth_options -effort high -carry none
 synthesize delay
@@ -13,6 +14,6 @@ packing
 global_placement
 place
 route
-sta
+sta opensta
 power
 bitstream
