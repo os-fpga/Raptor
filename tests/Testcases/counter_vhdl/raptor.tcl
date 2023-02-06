373c691a35aba595bbefa52daa66e34bcb2eeb03
alain-rs <90641725+alain-rs@users.noreply.github.com>
2023-02-05 20:00:00 -0800

    Raptor 1.5.12 (#622)
    * Sprint 13.2
    
    * Sprint 13.2
    
    * raptor_1.5.12 + 1GE75 in tests
    
    * set_property help
    
    * help
    
    ---------
    
    Co-authored-by: Alain Dargelas <alain.dargelas@gmail.com>

--- tests/Testcases/counter_vhdl/raptor.tcl
+++ tests/Testcases/counter_vhdl/raptor.tcl
@@ -10,7 +10,7 @@ add_simulation_file -VHDL_1993 testbench.vhd
 set_top_testbench tb_counters
 
 # Device target
-target_device GEMINI_10x8
+target_device 1GE75
 
 # Compilation/Simulation
 analyze
--- tests/Testcases/mult_gemini/raptor.tcl
+++ tests/Testcases/mult_gemini/raptor.tcl
@@ -1,12 +1,12 @@
 # Design
-create_design and2_gemini
-add_design_file -V_2001 ./rtl/and2.v
-set_top_module and2
-add_constraint_file pin_mapping.pin
+create_design and2_gemini_latest
+add_design_file -V_2001 mult.v
+set_top_module dsp_mult
 add_constraint_file constraints.sdc
 
 # Device target
-target_device 1GE75
+target_device GEMINI
+
 # Compilation
 analyze
 synthesize delay
