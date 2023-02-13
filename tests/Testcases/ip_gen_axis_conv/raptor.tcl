3a7ebf2fdcb23765e2d134b8098b73a36df076ca
Bilal Ahmed <bilal.ahmed@rslpt44.rapidsilicon.local>
2023-02-13 12:06:04 +0500

    Ip_gen_Axis_conv example fixed

--- tests/Testcases/ip_gen_axis_conv/raptor.tcl
+++ tests/Testcases/ip_gen_axis_conv/raptor.tcl
@@ -1,15 +1,6 @@
 create_design ip_test
 target_device 1GE75
-add_litex_ip_catalog ip_generators/
-puts "Catalog:"
-foreach ip [ip_catalog] {
-    puts "IP: $ip"
-    foreach param [ip_catalog $ip] {
-        puts "  PARAM [lindex $param 0], default: [lindex $param 1]"
-    }
-}
-
-configure_ip axis_converter -mod_name conv32_16 -version 1.0 -Pcore_in_width=32 -Pcore_out_width=16 -Pcore_reverse=0 -out_file rs_ips/conv32_16.v
+configure_ip axis_width_converter_v1_0 -mod_name conv32_16 -version 1.0 -Pcore_in_width=32 -Pcore_out_width=16 -Pcore_reverse=0 -out_file rs_ips/conv32_16.v
 ipgenerate
 set_top_module use_ip
 add_design_file rs_ips/conv32_16.v use_ip.v
