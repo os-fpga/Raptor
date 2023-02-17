e394d9a76b4715e72587e3554da7e91f14c523d6
Alain Dargelas <alain.dargelas@gmail.com>
2023-02-17 10:40:07 -0800

    1GE100 part

---
+++ tests/Testcases/vex_soc/raptor_vex_100K.tcl
@@ -0,0 +1,18 @@
+create_design vex_100K
+target_device 1GE100
+add_include_path ./
+add_library_path rtl/
+add_library_ext .v .sv
+add_design_file rtl/vex_soc.v
+set_top_module vex_soc
+
+analyze 
+synth_options -effort high -carry all 
+synthesize delay
+packing
+global_placement
+place
+route
+sta 
+power
+bitstream
