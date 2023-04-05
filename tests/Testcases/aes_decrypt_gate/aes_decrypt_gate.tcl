b4fcf2ef6cb52ad90bcf50b3413a2c198057a342
alain-rs <90641725+alain-rs@users.noreply.github.com>
2023-04-05 11:40:03 -0700

    Change target device in examples (#775)
    Co-authored-by: Alain Dargelas <alain.dargelas@gmail.com>

--- tests/Testcases/aes_decrypt_gate/aes_decrypt_gate.tcl
+++ tests/Testcases/aes_decrypt_gate/aes_decrypt_gate.tcl
@@ -1,6 +1,6 @@
 # Gate level netlist input
 create_design AES_DECRYPT_GATE -type gate-level
-target_device 1GE75 
+target_device 1GE100
 read_netlist aes_decrypt.blif
 pnr_netlist_lang blif
 add_constraint_file aes_decrypt_gate.sdc
