d37178f60900c126f960405a57120d9c9ff61f00
Nadeem Yaseen <86344264+nadeemyaseen-rs@users.noreply.github.com>
2023-05-05 21:59:19 +0500

    added another device files to copy (#846)

--- .github/copy_arch.sh
+++ .github/copy_arch.sh
@@ -10,3 +10,9 @@ cp openfpga-pd-castor-rs/k6n8_TSMC16nm_7.5T/FPGA10x8_gemini_compact_pnr/fabric_t
 cp openfpga-pd-castor-rs/k6n8_TSMC16nm_7.5T/FPGA10x8_gemini_compact_pnr/fabric_task/flow_inputs/fpga_repack_constraints.xml  etc/devices/gemini_compact_10x8/fpga_repack_constraints.xml
 cp openfpga-pd-castor-rs/k6n8_TSMC16nm_7.5T/FPGA10x8_gemini_compact_pnr/fabric_task/flow_inputs/openfpga_arch_annotated.xml etc/devices/gemini_compact_10x8/gemini_openfpga.xml
 cp openfpga-pd-castor-rs/k6n8_TSMC16nm_7.5T/FPGA10x8_gemini_compact_pnr/fabric_task/flow_inputs/output_fabric_key.xml etc/devices/gemini_compact_10x8/fabric_key.xml
+
+cp openfpga-pd-castor-rs/k6n8_TSMC16nm_7.5T/FPGA62x44_gemini_compact_pnr/fabric_task/flow_inputs/k6n8_vpr_annotated.xml etc/devices/gemini_compact_62x44/gemini_vpr.xml
+cp openfpga-pd-castor-rs/k6n8_TSMC16nm_7.5T/FPGA62x44_gemini_compact_pnr/fabric_task/flow_inputs/bitstream_setting.xml  etc/devices/gemini_compact_62x44/bitstream_setting.xml
+cp openfpga-pd-castor-rs/k6n8_TSMC16nm_7.5T/FPGA62x44_gemini_compact_pnr/fabric_task/flow_inputs/fpga_repack_constraints.xml  etc/devices/gemini_compact_62x44/fpga_repack_constraints.xml
+cp openfpga-pd-castor-rs/k6n8_TSMC16nm_7.5T/FPGA62x44_gemini_compact_pnr/fabric_task/flow_inputs/openfpga_arch_annotated.xml etc/devices/gemini_compact_62x44/gemini_openfpga.xml
+cp openfpga-pd-castor-rs/k6n8_TSMC16nm_7.5T/FPGA62x44_gemini_compact_pnr/fabric_task/flow_inputs/output_fabric_key.xml etc/devices/gemini_compact_62x44/fabric_key.xml
