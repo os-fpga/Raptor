72d7a7c94ba991d21d143491b8d6bde74a565f17
Alain Dargelas <alain.dargelas@gmail.com>
2023-05-05 16:31:28 -0700

    Icarus support

---
+++ tests/Testcases/constant/raptor.tcl
@@ -0,0 +1,21 @@
+create_design add__a_to_output
+target_device GEMINI_COMPACT_10x8
+add_include_path ./rtl
+add_library_path ./rtl
+add_library_ext .v .sv
+add_design_file add__a_to_output.v
+set_top_module add__a_to_output
+synthesize delay
+
+
+add_simulation_file  tb.v
+set_top_testbench tb
+
+simulate gate icarus syn_tb_pnr.fst
+
+packing
+global_placement
+place
+route
+
+simulate pnr icarus syn_tb_pnr.fst
