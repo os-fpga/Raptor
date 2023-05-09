5e04820a2b9686b585afcb1bc5b4a556790f3224
Alain Dargelas <alain.dargelas@gmail.com>
2023-05-09 15:31:06 -0700

    aes_decrypt_gate cleanup

--- tests/Testcases/aes_decrypt_gate/aes_decrypt_gate.tcl
+++ tests/Testcases/aes_decrypt_gate/aes_decrypt_gate.tcl
@@ -3,15 +3,11 @@ create_design AES_DECRYPT_GATE -type gate-level
 
 target_device 1GE75
 
-read_netlist aes_decrypt.v
-pnr_netlist_lang blif
+read_netlist aes_decrypt.blif
 add_constraint_file aes_decrypt_gate.sdc
 packing
 place
-pnr_options --router_lookahead classic
 route
 sta
-pnr_options
-power
 bitstream
 
