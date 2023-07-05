c72e1563e9b98876d4f950767730d52b80cd16a8
Volodymyr Kochyn <volodymyr.kochyn@rapidsilicon.com>
2023-07-05 18:11:10 +0300

    init resources, set cfd file

--- src/ProgrammerGui/main.cpp
+++ src/ProgrammerGui/main.cpp
@@ -21,12 +21,56 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #include <QApplication>
 
+#include "Compiler/CompilerOpenFPGA.h"
+#include "Compiler/TaskManager.h"
+#include "Configuration/CFGCompiler/CFGCompiler.h"
+#include "Main/Foedag.h"
 #include "ProgrammerGui/ProgrammerMain.h"
+#include "Utils/FileUtils.h"
 
-int main(int argc, char *argv[]) {
-  QApplication a(argc, argv);
-  FOEDAG::ProgrammerMain w;
-  w.setWindowTitle("Rapid Programmer");
-  w.show();
-  return a.exec();
+#define Company "Rapid Silicon"
+#define ToolName "Rapid Programmer"
+#define ExecutableName "raptor"
+
+QWidget* mainWindowBuilder(FOEDAG::Session* session) {
+  auto m = new FOEDAG::ProgrammerMain{};
+  m->setWindowTitle(ToolName);
+  std::filesystem::path dataDir =
+      session->GetCompiler()->GetConfigFileSearchDirectory();
+  auto files = FOEDAG::FileUtils::FindFilesByExtension(dataDir, ".cfg");
+  if (!files.empty())
+    m->setCfgFile(QString::fromStdString(files.front().string()));
+  return m;
+}
+
+void registerAllCommands(QWidget* widget, FOEDAG::Session* session) {
+  auto compiler = session->GetCompiler();
+  auto cfgCompiler = new FOEDAG::CFGCompiler(compiler);
+  compiler->SetConfiguration(cfgCompiler);
+  cfgCompiler->RegisterCommands(session->TclInterp(), false);
+}
+
+int main(int argc, char* argv[]) {
+  FOEDAG::CommandLine* cmd = new FOEDAG::CommandLine(argc, argv);
+  cmd->processArgs();
+
+  FOEDAG::GUI_TYPE guiType =
+      FOEDAG::Foedag::getGuiType(cmd->WithQt(), cmd->WithQml());
+
+  FOEDAG::ToolContext* context =
+      new FOEDAG::ToolContext(ToolName, Company, ExecutableName);
+
+  FOEDAG::Compiler* compiler = new FOEDAG::CompilerOpenFPGA;
+  auto taskM = new FOEDAG::TaskManager(compiler);
+  compiler->setTaskManager(taskM);
+  FOEDAG::Settings* settings = new FOEDAG::Settings();
+  FOEDAG::Foedag* foedag = new FOEDAG::Foedag(
+      cmd, mainWindowBuilder, registerAllCommands, compiler, settings, context);
+  std::filesystem::path binpath = foedag->Context()->BinaryPath();
+  std::filesystem::path openOcdPath = binpath / "openocd";
+  std::filesystem::path datapath = foedag->Context()->DataPath();
+  compiler->ProgrammerToolExecPath(openOcdPath);
+  std::filesystem::path configFileSearchDir = datapath / "configuration";
+  compiler->SetConfigFileSearchDirectory(configFileSearchDir);
+  return foedag->init(guiType);
 }
