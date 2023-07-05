7a5a796ac7aa3ae4240814ae1551adad6ed265ad
Volodymyr Kochyn <volodymyr.kochyn@rapidsilicon.com>
2023-07-05 18:11:10 +0300

    init resources, set cfd file

--- src/ProgrammerGui/main.cpp
+++ src/ProgrammerGui/main.cpp
@@ -20,13 +20,46 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 #include <QApplication>
+#include <filesystem>
 
+#include "Configuration/CFGCompiler/CFGCompiler.h"
+#include "Main/Foedag.h"
 #include "ProgrammerGui/ProgrammerMain.h"
+#include "Utils/FileUtils.h"
 
-int main(int argc, char *argv[]) {
-  QApplication a(argc, argv);
-  FOEDAG::ProgrammerMain w;
-  w.setWindowTitle("Rapid Programmer");
-  w.show();
-  return a.exec();
+QWidget* mainWindowBuilder(FOEDAG::Session* session) {
+  auto m = new FOEDAG::ProgrammerMain{};
+  m->setWindowTitle("Rapid Programmer");
+
+  std::filesystem::path exeDirPath =
+      QCoreApplication::applicationDirPath().toStdString();
+  std::filesystem::path installDir = exeDirPath.parent_path();
+  std::filesystem::path dataDir =
+      installDir / "share" / "raptor" / "configuration";
+  auto files = FOEDAG::FileUtils::FindFilesByExtension(dataDir, ".cfg");
+  if (!files.empty())
+    m->setCfgFile(QString::fromStdString(files.front().string()));
+
+  return m;
+}
+
+void registerAllCommands(QWidget* widget, FOEDAG::Session* session) {
+  auto compiler = session->GetCompiler();
+  auto cfgCompiler = new FOEDAG::CFGCompiler(compiler);
+  compiler->SetConfiguration(cfgCompiler);
+  cfgCompiler->RegisterCommands(session->TclInterp(), false);
+}
+
+int main(int argc, char* argv[]) {
+  FOEDAG::CommandLine* cmd = new FOEDAG::CommandLine(argc, argv);
+  cmd->processArgs();
+
+  FOEDAG::GUI_TYPE guiType =
+      FOEDAG::Foedag::getGuiType(cmd->WithQt(), cmd->WithQml());
+
+  FOEDAG::Compiler* compiler = new FOEDAG::Compiler;
+  FOEDAG::Settings* settings = new FOEDAG::Settings();
+  FOEDAG::Foedag* foedag = new FOEDAG::Foedag(
+      cmd, mainWindowBuilder, registerAllCommands, compiler, settings);
+  return foedag->init(guiType);
 }
