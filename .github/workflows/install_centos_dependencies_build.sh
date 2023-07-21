280b0828c40006242fc315742d496fff127e7dfb
Nadeem Yaseen <86344264+nadeemyaseen-rs@users.noreply.github.com>
2023-07-21 22:26:58 +0500

    Update install_centos_dependencies_build.sh to have new way to get QT

--- .github/workflows/install_centos_dependencies_build.sh
+++ .github/workflows/install_centos_dependencies_build.sh
@@ -55,37 +55,10 @@ echo 'QMAKE_CC=/opt/rh/devtoolset-11/root/usr/bin/gcc' >> $GITHUB_ENV
 echo 'QMAKE_CXX=/opt/rh/devtoolset-11/root/usr/bin/g++' >> $GITHUB_ENV
 echo 'PATH=/usr/local/Qt-5.15.4/bin:/usr/lib/ccache:'"$PATH" >> $GITHUB_ENV
 
-if [ -f buildqt5-centos7-gcc.zip ]
-then
-  echo "Found QT build artifact, untarring..."
-  unzip buildqt5-centos7-gcc.zip
-  tar xvzf buildqt5-centos7-gcc.tgz
-fi
-
 echo "Downloading QT..."
-curl -L https://download.qt.io/official_releases/qt/5.15/5.15.4/single/qt-everywhere-opensource-src-5.15.4.tar.xz --output qt-everywhere-src-5.15.4.tar.xz
-tar -xf qt-everywhere-src-5.15.4.tar.xz
-
-if [ -d "buildqt5" ] 
-then
-  echo "Installing QT..."
-  cd buildqt5  
-  make install
-  cd ..
-else
-  echo "Building QT..."
-  # work around to make it complie on GCC 11. For reference, see (https://forum.qt.io/topic/139626/unable-to-build-static-version-of-qt-5-15-2/13)
-  sed -i '44i\#include <limits>' qt-everywhere-src-5.15.4/qtbase/src/corelib/text/qbytearraymatcher.h
-  sed -i '52i\#include <limits>' qt-everywhere-src-5.15.4/qtdeclarative/src/qmldebug/qqmlprofilerevent_p.h
-  cat qt-everywhere-src-5.15.4/qtbase/src/corelib/text/qbytearraymatcher.h
-  mkdir buildqt5
-  cd buildqt5
-  source /opt/rh/devtoolset-11/enable
-  ../qt-everywhere-src-5.15.4/configure -opensource -confirm-license -xcb -xcb-xlib -bundled-xcb-xinput -no-compile-examples -nomake examples
-  make -j `nproc`
-  echo "Installing QT..."
-  make install
-  cd ..
-fi
+curl -L https://github.com/RapidSilicon/post_build_artifacts/releases/download/v0.1/Qt_5.15.4.tar.gz --output qt-everywhere-src-5.15.4.tar.gz
+tar -xzf qt-everywhere-src-5.15.4.tar.gz
+mv 5.15.4 /usr/local/Qt-5.15.4
 yum clean all
-rm -rf qt-everywhere-src-5.15.4.tar.xz qt-everywhere-src-5.15.4
+rm -rf qt-everywhere-src-5.15.4.tar.gz
+
