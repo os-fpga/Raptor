7f20b994e9cd14fba92d40971d9352c6b15ab881
behzadmehmood-rs <behzad.mahmood@rapidsilicon.com>
2023-08-09 16:38:41 +0500

    Updating tcl files for io_testcases

--- tests/io_testcases/SERDES_DESIGN/raptor.tcl
+++ tests/io_testcases/SERDES_DESIGN/raptor.tcl
@@ -2,10 +2,9 @@ set project_name SERDES
 
 puts "Creating $project_name..."
 create_design $project_name -type gate-level
-target_device GEMINI_COMPACT_10x8
+target_device GEMINI_COMPACT_104x68
 pnr_netlist_lang verilog
 read_netlist serdes_post_synth.v
-#add_constraint_file i_serdes_1_to_4.sdc
 
 # Compilation
 puts "Compiling $project_name..."
