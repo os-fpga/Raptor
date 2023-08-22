f31216a84f2408b6762970e51067f786739aa15b
Guan Yung <115125680+guanyung-rs@users.noreply.github.com>
2023-08-23 04:00:49 +0800

    Update FOEDAG_rs programmer lib API and CI build (#1111)
    * Update FOEDAG_rs programmer lib API and CI build
    
    * Update FOEDAG_rs clangtidy build
    
    * Update FOEDAG_rs
    
    * Update foedag_rs
    
    * Fix extra yum install
    
    ---------
    
    Co-authored-by: ravi-rs <ravi-rs@users.noreply.github.com>

--- .github/workflows/install_macos_dependencies_build.sh
+++ .github/workflows/install_macos_dependencies_build.sh
@@ -8,7 +8,8 @@ brew install bison \
     readline \
     ninja \
     wget \
-    coreutils
+    coreutils \
+    libusb
 export PATH="/usr/local/opt/qt@5/bin:$PATH"
 export LDFLAGS="-L/usr/local/opt/qt@5/lib"
 export CPPFLAGS="-I/usr/local/opt/qt@5/include"
