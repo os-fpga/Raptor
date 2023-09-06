f1c7b88c0e72207163208b9d2ecaf18c969dbbef
Alain Dargelas <90641725+alain-rs@users.noreply.github.com>
2023-09-05 18:10:07 -0700

    Update surelog (#1160)
    * Update Surelog and reactivate a test
    
    * Update Surelog and reactivate a test
    
    * Update Surelog and reactivate a test
    
    ---------
    
    Co-authored-by: Alain Dargelas <alain.dargelas@gmail.com>

--- tests/TestIP/axi_interconnect/v1_0/axi_interconnect.tcl
+++ tests/TestIP/axi_interconnect/v1_0/axi_interconnect.tcl
@@ -3,14 +3,16 @@ create_design axi_interconnect_prj
 target_device 1GE100
 
 #Configuring axi_interconnect_v1_0 IP 
-configure_ip axi_interconnect_v1_0 -mod_name axi_interconnect_wrapper -Pdata_width=32 -Paddr_width=8 -out_file ./axi_interconnect_wrapper.v
+configure_ip axi_interconnect_v1_0 -mod_name axi_interconnect_wrapper -Pdata_width=32 -Paddr_width=32 -out_file ./axi_interconnect_wrapper.v
 
 #Generate IP
 ipgenerate
 
 #Add generate IP as a design and synthesize it
 add_design_file ./rapidsilicon/ip/axi_interconnect/v1_0/axi_interconnect_wrapper/src/axi_interconnect_wrapper.v
-add_library_path rapidsilicon/ip/axi_interconnect/v1_0/axi_interconnect_wrapper/src/
+add_design_file ./rapidsilicon/ip/axi_interconnect/v1_0/axi_interconnect_wrapper/src/axi_interconnect.v
+add_design_file ./rapidsilicon/ip/axi_interconnect/v1_0/axi_interconnect_wrapper/src/arbiter.v
+add_design_file ./rapidsilicon/ip/axi_interconnect/v1_0/axi_interconnect_wrapper/src/priority_encoder.v
 set_top_module axi_interconnect_wrapper
 parser_type surelog
 synth delay
