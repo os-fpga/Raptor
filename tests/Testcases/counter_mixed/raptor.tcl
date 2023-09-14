4bd52e0cf0e3846c74f19cff3be11f8348b7c8aa
Alain Dargelas <90641725+alain-rs@users.noreply.github.com>
2023-09-13 22:19:07 -0700

    mixed lang (#1184)
    Co-authored-by: Alain Dargelas <alain.dargelas@gmail.com>

---
+++ tests/Testcases/counter_mixed/raptor.tcl
@@ -0,0 +1,43 @@
+# Design
+create_design counter_mixed
+add_design_file -VHDL_1993 UP_COUNTER.vhd
+add_design_file -SV_2012 counter.v
+set_top_module counter
+#add_constraint_file pin_mapping.pin
+add_constraint_file constraints.sdc
+
+# Testbench
+add_simulation_file -VHDL_1993 testbench.vhd
+set_top_testbench tb_counters
+
+# Device target
+target_device GEMINI_COMPACT_10x8
+
+# Compilation/Simulation
+analyze
+
+# RTL Simulation
+simulation_options "ghdl" "simulation" "--stop-time=1000ns"
+#simulate "rtl" "ghdl" syn_tb_rtl.fst
+
+# Synthesis
+parser_type ghdl
+#pnr_netlist_lang vhdl
+synthesize delay
+
+# Post-Synthesis gate-level Simulation
+#simulate "gate" "ghdl" syn_tb_gate.fst
+
+# Pack/Place/Route
+packing
+place
+route
+
+# Post-Route Simulation
+#simulate pnr ghdl syn_tb_pnr.fst
+
+# Static Timing Analysis
+sta 
+
+# Bitstream Generation
+bitstream 
