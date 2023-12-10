84b70fddcf4a3e6efe754c7b48e74d3c1979ac51
nadeemyaseen-rs <nadeem.yaseen@rapidsilicon.com>
2023-12-10 16:59:54 +0000

    Added ric files from 0.9.19

---
+++ etc/devices/gemini_compact_62x44/ric/root_mux.tcl
@@ -0,0 +1,24 @@
+#!/usr/bin/env tclsh
+####################################
+# created by Alex H , June 26, 2023
+# updated by George C, Oct 2023
+# ROOT_MUX definition 
+####################################
+define_block -name ROOT_MUX
+
+####################################
+# define configuration attributes (parameters) 
+####################################
+define_param -block ROOT_MUX -name ROOT_MUX_SEL	-addr  0 -width 6 -type integer
+
+####################################
+# Constraints within block attributes 
+####################################
+
+# 52x1 mux
+######################################
+define_ports -block ROOT_MUX -out root_mux_clk_out
+for {set CLK_INDEX 0} {$CLK_INDEX <= 51} {incr CLK_INDEX} { 
+    define_ports -block ROOT_MUX -in root_clk_in_bit${CLK_INDEX}
+} 
+######################################
