4f32d13a493162fa1b6f37f3633716e9f2c8446c
alaindargelas <63669492+alaindargelas@users.noreply.github.com>
2024-02-24 21:19:25 -0800

    Update gate-level example (#1541)

--- tests/tcl_examples/aes_decrypt_gate/run_raptor.tcl
+++ tests/tcl_examples/aes_decrypt_gate/run_raptor.tcl
@@ -10,16 +10,23 @@ set project_name AES_DECRYPT_GATE
 
 puts "Creating $project_name..."
 create_design $project_name -type gate-level
-target_device 1GE75
+target_device 1VG28
 read_netlist decrypt_top.v
 add_constraint_file constraints.sdc
 
 # Compilation
 puts "Compiling $project_name..."
+
+analyze
+
+# Converts Verilog to Eblif
+synthesize
+
 packing
 place
 route
 sta
+power
 # bitstream
 
 puts "Completed run_raptor.tcl\n"
