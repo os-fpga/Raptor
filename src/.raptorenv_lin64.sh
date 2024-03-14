3a4ddedf5fea488415ebd86e3d53b81609d940d0
alaindargelas <63669492+alaindargelas@users.noreply.github.com>
2024-03-14 10:01:36 -0700

    Fix simulation setup (#1590)

--- src/.raptorenv_lin64.sh
+++ src/.raptorenv_lin64.sh
@@ -26,7 +26,7 @@ else
 	export PYTHONPATH=$RAPTOR_PATH/share/litex_reference_designs/:$RAPTOR_PATH/share/raptor/IP_Catalog/
 fi
 # Contains bogus VERILATOR_ROOT
-#[[ -f "$RAPTOR_PATH/bin/HDL_simulator/setup_sim" ]] && source $RAPTOR_PATH/bin/HDL_simulator/setup_sim
+[[ -f "$RAPTOR_PATH/bin/HDL_simulator/setup_sim" ]] && source $RAPTOR_PATH/bin/HDL_simulator/setup_sim
 
 # uncomment to open the debug
 #export QT_DEBUG_PLUGINS=1
@@ -36,20 +36,3 @@ fi
 
 export PATH=$RAPTOR_PATH/share/envs/litex/bin:$PATH
 export LIBPYTHON_LOC=$RAPTOR_PATH/share/envs/python3.8/lib/libpython3.8.so.1.0
-
-TOP_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )/bin/HDL_simulator"
-ostype=`egrep '^(NAME)=' /etc/os-release  | grep -o -e Ubuntu -e CentOS`
-if [ "$ostype" == "CentOS" ]
-then
-export PATH=$TOP_DIR/gcc-11.4.0/bin:$PATH
-export LD_LIBRARY_PATH=$TOP_DIR/gcc-11.4.0/lib64:$LD_LIBRARY_PATH
-fi
-echo "$TOP_DIR/GHDL/lib"
-export LD_LIBRARY_PATH=$TOP_DIR/GHDL/lib:$LD_LIBRARY_PATH
-export SYSTEMC_INCLUDE=$TOP_DIR/systemc_2.3.3/include
-export SYSTEMC_LIBDIR=$TOP_DIR/systemc_2.3.3/lib-linux64
-export LD_LIBRARY_PATH=$TOP_DIR/systemc_2.3.3/lib-linux64:$LD_LIBRARY_PATH
-export PATH=$TOP_DIR/verilator/bin:$PATH
-export LIBRARY_PATH=/usr/lib/x86_64-linux-gnu:$LIBRARY_PATH
-export PATH=$TOP_DIR/GHDL/bin:$PATH
-export PATH=$TOP_DIR/iverilog/bin:$PATH
