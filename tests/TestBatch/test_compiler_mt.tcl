8ee1fc0df7884012d4dd6a84e74d461dd0612d8c
volodymyrkochyn <vovakochun@gmail.com>
2024-06-13 21:05:57 +0300

    Fixed tcl command (#1799)

--- tests/TestBatch/test_compiler_mt.tcl
+++ tests/TestBatch/test_compiler_mt.tcl
@@ -16,7 +16,7 @@
 
 #You should have received a copy of the GNU General Public License
 #along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
+create_design test
 synthesize
 after 3000 {puts "STOP"; flush stdout ; stop} 
 after 5000 {synthesize}
