926cf1171dd58c8d260328260549befd2d6ab2da
NadeemYaseen <pu.engineerg@gmail.com>
2024-09-27 00:26:26 +0500

    more strict conditions

--- asset_fetcher.cmake
+++ asset_fetcher.cmake
@@ -7,7 +7,7 @@ function(BriningCommercialDevices)
     else()
         message(FATAL_ERROR "Devices directory not found: ${DEVICES_DIR}")
     endif()
-    message(STATUS "Fetching Assests from ${REPO}")
+    message(STATUS "Fetching Assets from ${REPO}")
     execute_process(
         COMMAND git clone --filter=blob:none --no-checkout https://${TOKEN}@github.com/${REPO}.git ${SRC}
         WORKING_DIRECTORY ${CMAKE_CURRENT_LIST_DIR}
@@ -41,10 +41,21 @@ function(BriningCommercialDevices)
     endif()
 endfunction()
 
+
+function(extract_ini_value key pattern var_name)
+    string(REGEX MATCH "${key}=(${pattern})" match "${ini_content}")
+    if(NOT match)
+        message(FATAL_ERROR "Error: ${key} is missing or could not be parsed from the .ini file")
+    else()
+        string(REGEX REPLACE "${key}=(${pattern})" "\\1" extracted_value "${match}")
+        set(${var_name} "${extracted_value}" PARENT_SCOPE)  # Set variable in parent scope
+    endif()
+endfunction()
+
 if(NOT EXISTS "${CMAKE_CURRENT_LIST_DIR}/etc/devices/etc/devices")
 
     # Set the path to the .ini file in $HOME/.local
-    set(INI_FILE "$ENV{HOME}/.local/repator-dev.ini")
+    set(INI_FILE "$ENV{HOME}/.local/raptor-dev.ini")
 
     # Check if the .ini file exists
     if(NOT EXISTS "${INI_FILE}")
@@ -53,20 +64,13 @@ if(NOT EXISTS "${CMAKE_CURRENT_LIST_DIR}/etc/devices/etc/devices")
         # Read the contents of the .ini file
         file(READ "${INI_FILE}" ini_content)
 
-        # Extract TOKEN, REPO, and SRC using regex
-        string(REGEX MATCH "TOKEN=([a-zA-Z0-9_]+)" token_match "${ini_content}")
-        string(REGEX REPLACE "TOKEN=([a-zA-Z0-9_]+)" "\\1" TOKEN "${token_match}")
-
-        string(REGEX MATCH "REPO=([a-zA-Z0-9/_-]+)" repo_match "${ini_content}")
-        string(REGEX REPLACE "REPO=([a-zA-Z0-9/_-]+)" "\\1" REPO "${repo_match}")
-
-        string(REGEX MATCH "SRC=([a-zA-Z0-9/_-]+)" src_match "${ini_content}")
-        string(REGEX REPLACE "SRC=([a-zA-Z0-9/_-]+)" "\\1" SRC "${src_match}")
-
+        # Extract TOKEN, REPO, and SRC using extract_ini_value 
+        extract_ini_value("TOKEN" "[a-zA-Z0-9_]+" TOKEN)
+        extract_ini_value("REPO" "[a-zA-Z0-9/_-]+" REPO)
+        extract_ini_value("SRC" "[a-zA-Z0-9/_-]+" SRC)
         # Output the extracted values (for debug purposes)
-        #message("GitHub Token: ${TOKEN}")
-        #message("GitHub Repo: ${REPO}")
-        #message("Source Path: ${SRC}")
+        message(STATUS "Parsed values: TOKEN=${TOKEN}, REPO=${REPO}, SRC=${SRC}")
+ 
         # call function to populate Commercial devices
         BriningCommercialDevices()
     endif()
